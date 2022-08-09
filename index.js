89e9cb142373de2a67155abbd5665f26c1dabcf8
xaster-Kies <samueledward40@gmail.com>
2022-08-10 00:59:28 +0100

    FEAT: blank style of the terminal

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const figlet = require('figlet')
 
 // utility package import
 const files = require('./lib/files')
+
+
+// Styles for the blank console
+clear();
+console.log(
+  chalk.green(
+    figlet.textSync('R-init', { horizontalLayout: 'full' })
+  )
+)
+
+// Checks if a the particular repository exist already
+if (files.directoryExists('.git')) {
+  console.log(chalk.red('Already a git repository!'));
+  process.exit();
+}
